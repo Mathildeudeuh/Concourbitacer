d3d4dc68a20c0dd1d7ec074ec4a9e87ba57cb9ca
florie <f.poulet-souchard@etudiant.ecolemelies.fr>
2022-06-15 15:50:48 +0200

    Auto stash before merge of "main" and "origin/main"
    merge conflict solve

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,38 +5,33 @@ using TMPro;
 
 public class Score : MonoBehaviour
 {
-<<<<<<< Updated upstream
+
     public static Score instance;
-=======
->>>>>>> Stashed changes
+
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI highscoreText;
 
     int score = 0;
     int highscore = 0;
 
-<<<<<<< Updated upstream
+
     public void Awake()
     {
         instance = this;
     }
 
-=======
->>>>>>> Stashed changes
+
     private void Start()
     {
         scoreText.text = score.ToString() + "POINTS";
         highscoreText.text = "HIGHSCORE:" + highscore.ToString();
     }
 
-<<<<<<< Updated upstream
     public void AddPoint()
     {
         score += 1;
         scoreText.text = score.ToString() + "POINTS";
 
     }
-=======
 
->>>>>>> Stashed changes
 }
