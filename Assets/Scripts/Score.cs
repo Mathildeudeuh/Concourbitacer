36d24b5ffc151a12e6f3fc96cb918bd5f357e346
Mathilde Degris <m.degris@etudiant.ecolemelies.fr>
2022-06-16 15:34:09 +0200

    Code Spawn plante à partir d'un certain score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -8,13 +8,16 @@ public class Score : MonoBehaviour
 
     public static Score instance;
 
-    //private Arroser arroser;
-
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI highscoreText;
 
     int score = 0;
     int highscore = 0;
+
+    public GameObject spawner;
+    public GameObject plante;
+    public GameObject plante1;
+
 
 
     public void Awake()
@@ -35,4 +38,32 @@ public class Score : MonoBehaviour
         scoreText.text = "POINTS : " + score.ToString();
     }
 
+    public void SpawnPlante()
+    {
+        var pointEngrais = FindObjectOfType<Nourrir>();
+        var pointEau = FindObjectOfType<Arroser>();
+
+        if (pointEngrais.scoreEngrais >= 10)
+        {
+            if (pointEau.scoreEau >= 10)
+            {
+                Instantiate(plante, spawner.transform.position, spawner.transform.rotation);
+            }
+        }
+
+        if (pointEngrais.scoreEngrais >= 30)
+        {
+            if (pointEau.scoreEau >= 30)
+            {
+                Destroy(plante);
+                Instantiate(plante1, spawner.transform.position, spawner.transform.rotation);
+            }
+
+        }
+    }
+
+    private void Update()
+    {
+        SpawnPlante();
+    }
 }
